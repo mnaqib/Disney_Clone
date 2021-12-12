821c6614098857521bab0913268d9ef7619d2872
mnaqib <naqibdargaroad@gmail.com>
2021-12-12 23:53:55 +0530

    viewers

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -21,6 +21,12 @@ const ImgSlider = () => {
       <Wrap>
         <img src="/images/slider-badag.jpg" alt="" />
       </Wrap>
+      <Wrap>
+        <img src="/images/slider-scale.jpg" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-scales.jpg" alt="" />
+      </Wrap>
     </Carousel>
   );
 };
